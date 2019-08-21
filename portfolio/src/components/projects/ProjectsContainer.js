cabca849fae8fc8727ff46dd8d1f080dc6ac48b7
VulpineReynard <milare8@gmail.com>
2019-08-21 19:34:48 -0400

    everything works again

--- portfolio/src/components/projects/ProjectsContainer.js
+++ portfolio/src/components/projects/ProjectsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProjectsNav from './ProjectsNav'
 
 const ProjectsContainer = () => {
   return (
---
+++ portfolio/src/components/projects/ProjectsNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsNav = () => {
+  return (
+    <div className="projects-nav">
+
+    </div>
+  );
+}
+
+export default ProjectsNav;
